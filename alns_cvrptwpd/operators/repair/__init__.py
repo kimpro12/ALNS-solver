22801f6e8e24ff8cb84f4d326a20812aa6f0f2eb
kimbro <95573884+kimpro12@users.noreply.github.com>
2025-10-31 18:37:31 +0000

    Revert "Add examples"
    This reverts commit ccac3310601e3f438e5073afe30a934d94a4df17.

--- alns_cvrptwpd/operators/repair/__init__.py
+++ alns_cvrptwpd/operators/repair/__init__.py
@@ -9,14 +9,3 @@ __all__ = [
     "random_top_k_insertion",
     "regret_insertion",
 ]
-"""Repair operator implementations."""
-
-from .best_insertion import best_insertion
-from .random_top_k_insertion import random_top_k_insertion
-from .regret_insertion import regret_insertion
-
-__all__ = [
-    "best_insertion",
-    "random_top_k_insertion",
-    "regret_insertion",
-]
